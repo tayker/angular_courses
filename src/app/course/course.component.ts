3afa6b2bfb4bdfefbd6f07b1a00faee4c2c55213
AlexSumbit <alexsumbit@gmail.com>
2018-07-10 22:59:31 +0300

    timer fix, video fix

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalService } from '../modal.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-course',
@@ -9,29 +10,34 @@ import { ModalService } from '../modal.service';
 export class CourseComponent implements OnInit {
 
   @Input() course;
-  countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
-  now: any = new Date().getTime();
-  distance = this.countDownDate - this.now;
-  days: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
   constructor(
-    private modalService: ModalService
+    private modalService: ModalService,
+    private sanitizer: DomSanitizer
   ) { }
 
-  timer(){
-	
-	this.days = Math.floor(this.distance / (1000 * 60 * 60 * 24));
-    this.hours = Math.floor((this.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    this.minutes = Math.floor((this.distance % (1000 * 60 * 60)) / (1000 * 60));
-    this.seconds = Math.floor((this.distance % (1000 * 60)) / 1000);
+  timer = {}
+
+  getTimer(){
+    let countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+    let now: any = new Date().getTime();
+    let distance = countDownDate - now;
+
+    return {
+      days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((distance % (1000 * 60)) / 1000)
+    }
   }
   onBuyBtnClick() {
     this.modalService.openModal("buy", this.course);
   }
   ngOnInit() {
-	  this.timer()
+    this.course.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + this.course.videoUrl);
+    this.timer = this.getTimer();
+	  setInterval(() => {
+      this.timer = this.getTimer();
+    }, 1000);
   }
 
 }
